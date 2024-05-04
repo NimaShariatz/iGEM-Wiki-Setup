679a7374ab150343c195910c65060d16380fb15e
Yanmachine <hardingiant@gmail.com>
2024-05-04 00:41:58 -0600

    Progress Bar
    progress bar tracker at the top of screen

---
+++ progressBar.js
@@ -0,0 +1,21 @@
+var article = document.querySelector('article');
+
+function updateProgressBar() {
+    //retreive total scroll height of the article
+    var scrollTotal = article.scrollHeight - window.innerHeight;
+
+    //retire current scroll location
+    var scrollSpot = article.scrollTop;
+    console.log("Scroll spot:", scrollSpot + " max scroll heigh ", scrollTotal);
+
+    //Calulate percent of article scrolled, cap it to 100
+    var percentage = (scrollSpot / scrollTotal) * 100
+    if (percentage > 100){
+        percentage = 100;
+    }
+
+    //Add % to make it a percentage
+    document.querySelector('#progress-bar').style.setProperty('--scrollProgress', percentage+"%")
+}
+
+article.addEventListener('scroll', updateProgressBar);
