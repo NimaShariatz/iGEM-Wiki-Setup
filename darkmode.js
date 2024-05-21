e090be0d436a1ed13db7d2ec4b7b393866f3cf9d
Yanmachine <hardingiant@gmail.com>
2024-05-21 16:14:48 -0600

    theme swap delay
    Removed the delay from specific elements when changing the theme

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,29 @@
-var toggle = document.getElementById("toggle-mode")
+// var toggle = document.getElementById("toggle-mode")
 
-toggle.onclick = function(){
-    document.body.classList.toggle("dark-theme")
-}
+// toggle.onclick = function(){
+//     document.body.classList.toggle("dark-theme")
+// }
+
+var toggle = document.getElementById("toggle-mode");
+
+toggle.onclick = function() {
+    //SELECT ALL ELEMENTS
+    const allElements = document.querySelectorAll('*');
+
+    //add disable class
+    allElements.forEach(element => {
+        element.classList.add('no-transition');
+    });
+
+    document.body.classList.toggle("dark-theme");
+
+    //Ensures browser processes changes immediately
+    document.documentElement.offsetHeight;
+
+    //remove class after delay (Short)
+    setTimeout(() => {
+        allElements.forEach(element => {
+            element.classList.remove('no-transition');
+        });
+    }, 1);
+};
