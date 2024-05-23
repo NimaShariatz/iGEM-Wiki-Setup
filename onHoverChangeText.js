6d54976245c651231a4a7e36040cc93fa0a8be8e
NimaShariatz <shariatz@ualberta.ca>
2024-05-23 14:12:37 -0600

    improved menu hover text change

--- onHoverChangeText.js
+++ onHoverChangeText.js
@@ -3,24 +3,27 @@
 var header_menu = [document.getElementById("TEXT-project_menu"), document.getElementById("TEXT-wet_labmenu"), 
 document.getElementById("TEXT-dry_lab_menu"), document.getElementById("TEXT-wet_human_practices_menu"), document.getElementById("TEXT-Team_menu")];
 
-var original_text = ["<br/><br/>" , "<br/><br/>", "<br/><br/>",
-"<br/><br/>", "<br/><br/>"];
 
-var changed_text = ["A general outline of the project with emphasis on agriculture, crop infections, nanobodies and the relevancy of our research to this topic"];
+
+var changed_text = ["A general outline of the project with emphasis on agriculture, crop infections, nanobodies and the relevancy of our research to this topic"  
+];
 
 
 function mouseOver(header_menu, changed_text) {
 
-    header_menu.style.transition = "all 1s";
+    header_menu.style.transition = "opacity 0.5s linear";
     header_menu.innerHTML = changed_text;
     header_menu.style.opacity = "1";
+    header_menu.style.visibility = "visible";
+
 
   }
   
-  function mouseOut(header_menu, original_text) {
-      header_menu.style.transition = "all 1s";
-      header_menu.innerHTML = original_text;
-      header_menu.style.opacity = "0.6";
+  function mouseOut(header_menu) {
+      header_menu.style.transition = "visibility 0s 0.5s, opacity 0.5s linear";
+      header_menu.style.opacity = "0.0";
+      header_menu.style.visibility = "hidden";
+
   
   }
   
@@ -33,7 +36,7 @@ document.getElementById("P1").addEventListener("mouseover",function() {
 });
 
   document.getElementById("P1").addEventListener("mouseout",function() {
-    mouseOut(header_menu[0], original_text[0]);
+    mouseOut(header_menu[0]);
 });
 
 
@@ -42,7 +45,7 @@ document.getElementById("P2").addEventListener("mouseover",function() {
 });
 
   document.getElementById("P2").addEventListener("mouseout",function() {
-    mouseOut(header_menu[0], original_text[0]);
+    mouseOut(header_menu[0]);
 });
 
 
@@ -51,7 +54,7 @@ document.getElementById("P3").addEventListener("mouseover",function() {
 });
 
   document.getElementById("P3").addEventListener("mouseout",function() {
-    mouseOut(header_menu[0], original_text[0]);
+    mouseOut(header_menu[0]);
 });
 
 
@@ -60,7 +63,7 @@ document.getElementById("P4").addEventListener("mouseover",function() {
 });
 
   document.getElementById("P4").addEventListener("mouseout",function() {
-    mouseOut(header_menu[0], original_text[0]);
+    mouseOut(header_menu[0]);
 });
 
 
@@ -72,7 +75,7 @@ document.getElementById("WL1").addEventListener("mouseover",function() {
 });
 
   document.getElementById("WL1").addEventListener("mouseout",function() {
-    mouseOut(header_menu[1], original_text[1]);
+    mouseOut(header_menu[1]);
 });
 
 document.getElementById("WL2").addEventListener("mouseover",function() {
@@ -80,7 +83,7 @@ document.getElementById("WL2").addEventListener("mouseover",function() {
 });
 
   document.getElementById("WL2").addEventListener("mouseout",function() {
-    mouseOut(header_menu[1], original_text[1]);
+    mouseOut(header_menu[1]);
 });
 
 document.getElementById("WL3").addEventListener("mouseover",function() {
@@ -88,7 +91,7 @@ document.getElementById("WL3").addEventListener("mouseover",function() {
 });
 
   document.getElementById("WL3").addEventListener("mouseout",function() {
-    mouseOut(header_menu[1], original_text[1]);
+    mouseOut(header_menu[1]);
 });
 
 document.getElementById("WL4").addEventListener("mouseover",function() {
@@ -96,7 +99,7 @@ document.getElementById("WL4").addEventListener("mouseover",function() {
 });
 
   document.getElementById("WL4").addEventListener("mouseout",function() {
-    mouseOut(header_menu[1], original_text[1]);
+    mouseOut(header_menu[1]);
 });
 
 
@@ -109,7 +112,7 @@ document.getElementById("DL1").addEventListener("mouseover",function() {
 });
 
   document.getElementById("DL1").addEventListener("mouseout",function() {
-    mouseOut(header_menu[2], original_text[2]);
+    mouseOut(header_menu[2]);
 });
 
 document.getElementById("DL2").addEventListener("mouseover",function() {
@@ -117,7 +120,7 @@ document.getElementById("DL2").addEventListener("mouseover",function() {
 });
 
   document.getElementById("DL2").addEventListener("mouseout",function() {
-    mouseOut(header_menu[2], original_text[2]);
+    mouseOut(header_menu[2]);
 });
 
 
@@ -129,7 +132,7 @@ document.getElementById("HP1").addEventListener("mouseover",function() {
 });
 
   document.getElementById("HP1").addEventListener("mouseout",function() {
-    mouseOut(header_menu[3], original_text[3]);
+    mouseOut(header_menu[3]);
 });
 
 document.getElementById("HP2").addEventListener("mouseover",function() {
@@ -137,7 +140,7 @@ document.getElementById("HP2").addEventListener("mouseover",function() {
 });
 
   document.getElementById("HP2").addEventListener("mouseout",function() {
-    mouseOut(header_menu[3], original_text[3]);
+    mouseOut(header_menu[3]);
 });
 
 document.getElementById("HP3").addEventListener("mouseover",function() {
@@ -145,7 +148,7 @@ document.getElementById("HP3").addEventListener("mouseover",function() {
 });
 
   document.getElementById("HP3").addEventListener("mouseout",function() {
-    mouseOut(header_menu[3], original_text[3]);
+    mouseOut(header_menu[3]);
 });
 
 document.getElementById("HP4").addEventListener("mouseover",function() {
@@ -153,7 +156,7 @@ document.getElementById("HP4").addEventListener("mouseover",function() {
 });
 
   document.getElementById("HP4").addEventListener("mouseout",function() {
-    mouseOut(header_menu[3], original_text[3]);
+    mouseOut(header_menu[3]);
 });
 
 document.getElementById("HP5").addEventListener("mouseover",function() {
@@ -161,7 +164,7 @@ document.getElementById("HP5").addEventListener("mouseover",function() {
 });
 
   document.getElementById("HP5").addEventListener("mouseout",function() {
-    mouseOut(header_menu[3], original_text[3]);
+    mouseOut(header_menu[3]);
 });
 
 
@@ -172,7 +175,7 @@ document.getElementById("T1").addEventListener("mouseover",function() {
 });
 
   document.getElementById("T1").addEventListener("mouseout",function() {
-    mouseOut(header_menu[4], original_text[4]);
+    mouseOut(header_menu[4]);
 });
 
 document.getElementById("T2").addEventListener("mouseover",function() {
@@ -180,5 +183,5 @@ document.getElementById("T2").addEventListener("mouseover",function() {
 });
 
   document.getElementById("T2").addEventListener("mouseout",function() {
-    mouseOut(header_menu[4], original_text[4]);
+    mouseOut(header_menu[4]);
 });
