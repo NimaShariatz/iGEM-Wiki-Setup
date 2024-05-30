08b84f155981be3b5bafc6e12ff3f5a6bbe05bd3
Yanmachine <hardingiant@gmail.com>
2024-05-29 23:05:14 -0600

    Min width description page

--- parallax.js
+++ parallax.js
@@ -4,9 +4,14 @@ let midground = document.getElementById('parallax-midground');
 let foreground = document.getElementById('parallax-foreground');
 let object = document .getElementById('parallax-object');
 
+let logo = document.getElementById('logo');
+
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     object.style.left = -value * 0.4 + 'px';
     foreground.style.top = -value  * 0.5 + 'px';
     midground.style.top = -value * 0.3 + 'px';
+
+
+    logo.style.top = -value * 0.4 + 'px';
 })
