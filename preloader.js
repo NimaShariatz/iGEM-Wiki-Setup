653596ebb5ac525c88581c88a84a169ca61d5af8
Yanmachine <hardingiant@gmail.com>
2024-05-30 16:08:42 -0600

    Attributions setup

--- preloader.js
+++ preloader.js
@@ -3,13 +3,13 @@
 var loader = document.getElementById("preloader");
 
 //Actual code:
-// window.addEventListener("load", function(){
-//     loader.style.display = "none";
-// })
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
 
 //NOTE: THe timeout is just to simulate how a loading page might work
-window.addEventListener("load", function(){
-    setTimeout(function() {
-        loader.style.display = "none";
-    }, 50);
-});
+// window.addEventListener("load", function(){
+//     setTimeout(function() {
+//         loader.style.display = "none";
+//     }, 50);
+// });
