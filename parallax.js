ac371f0522c83a93960d4ef15489be7027ebf39a
Yanmachine <hardingiant@gmail.com>
2024-05-29 22:27:06 -0600

    Parallax

---
+++ parallax.js
@@ -0,0 +1,12 @@
+let background = document.getElementById('parallax-background');
+let text = document.getElementById('parallax-text');
+let midground = document.getElementById('parallax-midground');
+let foreground = document.getElementById('parallax-foreground');
+let object = document .getElementById('parallax-object');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    object.style.left = -value * 0.4 + 'px';
+    foreground.style.top = -value  * 0.5 + 'px';
+    midground.style.top = -value * 0.3 + 'px';
+})
