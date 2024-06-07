fd8fa2f7adf9cdda7d040f14d09f5ea29c61b601
Yanmachine <hardingiant@gmail.com>
2024-06-06 23:30:09 -0600

    Local Storage

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,38 +1,65 @@
 "use strict";
 
-// var toggle = document.getElementById("toggle-mode")
+//on page load
+document.addEventListener("DOMContentLoaded", function() {
 
-// toggle.onclick = function(){
-//     document.body.classList.toggle("dark-theme")
-// }
+    const isDarkMode = localStorage.getItem('dark-theme') === 'true';
 
-var toggle = document.getElementById("toggle-mode");
-
-toggle.onclick = function() {
     //SELECT ALL ELEMENTS
     const allElements = document.querySelectorAll('*');
 
-    //add disable class
+
+    const toggle = document.getElementById("toggle-mode");
+
+
     allElements.forEach(element => {
         element.classList.add('no-transition');
     });
-
-    document.body.classList.toggle("dark-theme");
-
-    //Ensures browser processes changes immediately
-    document.documentElement.offsetHeight;
-
-    //remove class after delay (Short)
     setTimeout(() => {
         allElements.forEach(element => {
             element.classList.remove('no-transition');
         });
     }, 1);
-    if (document.body.classList.contains("dark-theme")) {
-        toggle.src = "images/sun.svg"; 
+
+    if (isDarkMode) {
+        document.body.classList.add("dark-theme");
+        toggle.src = "images/sun.svg";
         toggle.style.filter = "invert(100%)";
     } else {
-        toggle.src = "images/moon.svg"; 
+        toggle.src = "images/moon.svg";
         toggle.style.filter = "none";
     }
-};
+
+
+
+
+    //regular switch
+    toggle.onclick = function() {
+        //add a disable class
+        allElements.forEach(element => {
+            element.classList.add('no-transition');
+        });
+
+        document.body.classList.toggle("dark-theme");
+
+        //Ensures browser processes changes immediately
+        document.documentElement.offsetHeight;
+
+        //Remove class after delay (Short)
+        setTimeout(() => {
+            allElements.forEach(element => {
+                element.classList.remove('no-transition');
+            });
+        }, 1);
+
+        if (document.body.classList.contains("dark-theme")) {
+            localStorage.setItem('dark-theme', 'true');
+            toggle.src = "images/sun.svg";
+            toggle.style.filter = "invert(100%)";
+        } else {
+            localStorage.setItem('dark-theme', 'false');
+            toggle.src = "images/moon.svg";
+            toggle.style.filter = "none";
+        }
+    };
+});
